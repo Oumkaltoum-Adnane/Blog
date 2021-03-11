b7abfa1521d051682b27dee0c6ba69205277dfa3
kaltoum Adnane <kaltoum.adnane@gmail.com>
2021-03-11 17:18:50 +0100

    modif

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,35 +2,26 @@
 
 namespace App\DataFixtures;
 use App\Entity\Article;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-      //  création de 10 'faux' articles
-        for($i = 1 ; $i <= 10; $i++)
-        {
-          // Pour pouvoir insérer dans la table SQL 'Article', nous devons instancier un objet issu de cette classe
-            // L'entité 'Article' reflète la table SQL 'Article'
-            // Nous avons besoin de rensigner tout les setteurs et tout les objets $article afin de pouvoir générer les insertions en BDD
-          $article = new Article;
-          // On remplit les objets articles grace au setteurs
-          $article->setTitle("Titre de l'article n $i")
-                  ->setContent("<p>Contenu de l'article</p>")
-                  ->setImage("https://picsum.photos/600/400")
-                  ->setCreatedAt(new \DateTime);// on instancier la calsse datetime afin d'avoir automatiquement la date et l'heure dans la BDD
+      //j'importe la librairie Faker installée via composer
+      $faker = \Faker\Factory::create('fr_FR');
 
-          // En Symfony, nous avons besoin d'un manager qui permet de manipuler les lignes de la BDD (insertion, modification, suppression)
-            // persist() est une méthode issue de la classe ObjectManager qui permet de garder en mémoire les objets ârticle crées et préparer les requetes d'insertion (INSERT INTO)
-          $manager->persist($article);
-      
-        }
-        // flush() est une méthode issue de la classe ObjectManager qui permet véritablement d'executer les insertions en BDD (similaire à execute() en PHP)
-        $manager->flush();
-
-        // une fois les fixtures réaliseés, il faut les charger en BDD grace à doctrine (ORM) par la commande : 
-        // php bin/console doctrine:fixtures:load
+      //création de 3 catégorie
+      for($i = 1; $i <= 3 ; $i++)
+      {
+        $category = new Category;
+        $category->setTitle($faker->sentence()) 
+                ->setDescription($faker->paragraph());
+        $manager->persist($category);        
+                
+      }
     }
+
 }
